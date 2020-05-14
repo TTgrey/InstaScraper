85d7dd7616953e59fb82ad650eb8240f5c3b5ef4
unknown <sasha6615r@mail.ru>
2020-05-14 12:36:10 +0300

    added shebang

--- dataInsta.py
+++ dataInsta.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*- 
+
 import time
 import pandas as pd
 from scraper import InstaScrapper
--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*- 
+
 import selenium, time, re 
 from datetime import datetime
 from selenium.webdriver import Firefox
